9c9be9b97084445fce39a1868f4f1cb6cf0fee07
MMDIOUF <dioufmakhtar77@gmail.com>
2023-06-09 18:17:23 +0000

    first commit

---
+++ listess/lib/services/etudiant_service.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+
+import 'package:listess/models/etudiants_models.dart';
+
+class EtudiantService {
+  static final Dio _dio = Dio();
+
+  static Future<List<Etudiant>> getAllEtudiants() async {
+    try {
+      final response = await _dio.get('http://localhost:8080/api/inscription', queryParameters: {});
+      final List<dynamic> jsonData = response.data;
+      return jsonData.map((item) => Etudiant.fromJson(item)).toList();
+    } catch (e) {
+      throw Exception('Failed to fetch etudiants');
+    }
+  }
+
+  static Future<List<Etudiant>> getEtudiantsByClasse(String classe) async {
+    try {
+      final response = await _dio.get('http://localhost:8080/api/inscription', queryParameters: {'classe': classe});
+      final List<dynamic> jsonData = response.data;
+      return jsonData.map((item) => Etudiant.fromJson(item)).toList();
+    } catch (e) {
+      throw Exception('Failed to fetch etudiants by classe');
+    }
+  }
+}
+
+class Dio {
+  get(String s, {required Map<String, String> queryParameters}) {}
+}
